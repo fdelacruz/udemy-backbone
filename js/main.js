ec73e40602b43adfd43ee2df44864758d358bc8c
Francisco De La Cruz <helloquico@gmail.com>
2015-03-07 23:57:46 -0500

    Working with Collections

--- js/main.js
+++ js/main.js
@@ -4,16 +4,31 @@ var Songs = Backbone.Collection.extend({
   model: Song
 });
 
-var songs = new Songs([
-    new Song({ title: "Song 1"}),
-    new Song({ title: "Song 2"}),
-    new Song({ title: "Song 3"})
-]);
+var songs = new Songs();
 
-songs.add(new Song({ title: "Song 4" }));
+songs.add(new Song({ title: "Song 1", genre: "Jazz", downloads: 110 }), { at: 0 });
 
-var firstSong = songs.at(0);
+songs.push(new Song({ title: "Song 2", genre: "Jazz", downloads: 90 }));
 
-var songWithIdC1 = songs.get("c1");
+// var lastSong = songs.pop();
 
-songs.remove(firstSong);
+var jazzSongs = songs.where({ genre: "Jazz"});
+
+var firstJazzSong = songs.findWhere({ genre: "Jazz"});
+
+console.log("Jazz Songs", jazzSongs);
+
+console.log("First Jazz Song", firstJazzSong);
+
+var filteredSongs = songs.where({ genre: "Jazz", title: "Song 2" });
+console.log("Filtered Songs", filteredSongs);
+
+var topDownloads = songs.filter(function(song){
+  return song.get("downloads") > 100;
+});
+
+console.log("Top Downloads", topDownloads);
+
+songs.each(function(song){
+  console.log(song)
+});
