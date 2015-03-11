48a3c8010ca1ff0e5a9ada790be7ac377cf8a9e5
Francisco De La Cruz <helloquico@gmail.com>
2015-03-11 19:58:41 -0400

    Handling DOM Events

--- js/main.js
+++ js/main.js
@@ -1,39 +1,31 @@
 var Song = Backbone.Model.extend();
 
-var Songs = Backbone.Collection.extend({
-  model: Song
-});
-
 var SongView = Backbone.View.extend({
 
-  tagName: "li",
+  events: {
+    "click": "onClick",
+    "click .bookmark": "onClickBookmark"
+  },
+
+  onClick: function(){
+    console.log("Listened Clicked");
+  },
+
+  onClickBookmark: function(e){
+    e.stopPropagation();
+
+    console.log("Bookmark Clicked")
+  },
 
   render: function(){
-    this.$el.html(this.model.get("title"));
+    this.$el.html(this.model.get("title") +
+        " <button>Listen</button> <button class='bookmark'>Bookmark</button>");
 
     return this;
   }
 });
 
-var SongsView = Backbone.View.extend({
+var song = new  Song({ title: "Blue in Green" });
 
-  tagName: "ul",
-
-  render: function(){
-    var self = this;
-
-    this.model.each(function(song){
-      var songView = new SongView({ model: song });
-      self.$el.append(songView.render().$el);
-    });
-  }
-});
-
-var songs = new Songs([
-  new Song({ title: "Blue in Green" }),
-  new Song({ title: "So what" }),
-  new Song({ title: "All Blues" })
-]);
-
-var songsView = new SongsView({ el: "#songs", model: songs });
-songsView.render();
+var songView = new SongView({ el: "#container", model: song });
+songView.render();
