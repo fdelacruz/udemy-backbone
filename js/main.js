eca87d08fde165f8743f95d7a1b0c43b6c1c4a95
Francisco De La Cruz <helloquico@gmail.com>
2015-03-15 10:47:02 -0400

    Views Project - using "Best Practices" to remove DOM elements

--- js/main.js
+++ js/main.js
@@ -12,17 +12,24 @@ var VehicleView = Backbone.View.extend({
     "data-attribute": "data-color"
   },
 
+  initialize: function(){
+    this.model.on("destroy", this.remove, this); // 3. Listen for when the model is destroyed..
+  },
+
   events: {
-    "click .deleteVehicle": "onClickDelete"
+    "click .delete": "onClickDelete" // 1. Add click event..
   },
 
   onClickDelete: function(){
-    this.remove();
-    this.model.destroy();
+    this.model.destroy(); // 2. Call the Backbone.js destroy method on the model..
+  },
+
+  remove: function(){
+    this.$el.remove(); // 4. Call the jQuery remove method on the corresponding LI..
   },
 
   render: function(){
-    this.$el.html(this.model.get("vehicleMake") + " " + this.model.get("vehicleModel") + ", Registration Number: " + this.model.get("registrationNumber") + " <button class='deleteVehicle'>Delete</button>");
+    this.$el.html(this.model.get("vehicleMake") + " " + this.model.get("vehicleModel") + ", Registration Number: " + this.model.get("registrationNumber") + " <button class='delete'>Delete</button>");
 
     return this;
   }
