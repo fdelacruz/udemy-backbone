99bf592a239c204371a56410b1cc07f013fdec19
Francisco De La Cruz <helloquico@gmail.com>
2015-03-23 19:56:37 -0400

    Creating an Event Aggregator to Coordinate Multiple Views

--- js/main.js
+++ js/main.js
@@ -1,21 +1,83 @@
-var person = {
-  name: "Francisco",
+var Venue = Backbone.Model.extend();
 
-  walk: function() {
-    this.trigger("walking", {
-      speed: 1,
-      startTime: "08:00"
-    });
-  }
-};
-
-_.extend(person, Backbone.Events);
-
-person.on("walking", function(e){
-  console.log("Person is walking..");
-  console.log("Event Args", e);
+var Venues = Backbone.Collection.extend({
+  model: Venue
 });
 
-// person.off("walking");
+var VenueView = Backbone.View.extend({
+  tagName: "li",
 
-person.walk();
+  initialize: function(options) {
+    this.bus = options.bus;
+  },
+
+  events: {
+    "click": "onClick",
+  },
+
+  onClick: function(){
+    this.bus.trigger("venueSelected", this.model);
+  },
+
+  render: function(){
+    this.$el.html(this.model.get("name"));
+
+    return this;
+  }
+});
+
+var VenuesView = Backbone.View.extend({
+  tagName: "ul",
+
+  id: "venues",
+
+  initialize: function(options){
+    this.bus = options.bus;
+  },
+
+  render: function(){
+    var self = this;
+
+    this.model.each(function(venue){
+      var view = new VenueView({ model: venue, bus: self.bus });
+      self.$el.append(view.render().$el);
+    });
+
+    return this;
+  }
+});
+
+var MapView = Backbone.View.extend({
+  el: "#map-container",
+
+  initialize: function(options){
+    this.bus = options.bus;
+    this.bus.on("venueSelected", this.onVenueSelected, this);
+  },
+
+  onVenueSelected: function(venue){
+    this.model = venue;
+    this.render();
+  },
+
+  render: function(){
+    if (this.model)
+      this.$("#venue-name").html(this.model.get("name"));
+
+    return this;
+  }
+});
+
+var bus = _.extend({}, Backbone.Events);
+
+var venues = new Venues([
+    new Venue({ name: "30 Mill Espresso" }),
+    new Venue({ name: "Platform Espresso" }),
+    new Venue({ name: "Mr Foxx" })
+]);
+
+var venuesView = new VenuesView({ model: venues, bus: bus });
+$("#venues-container").html(venuesView.render().$el);
+
+var mapView = new MapView({ bus: bus });
+mapView.render();
