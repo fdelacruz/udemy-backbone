def969695af508306384611fad8ce76c76e85f48
Francisco De La Cruz <helloquico@gmail.com>
2015-03-30 17:59:16 -0400

    Toggling an Item

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -2,13 +2,27 @@ var TodoItemView = Backbone.View.extend({
   tagName: 'li',
 
   initialize: function(options) {
-    if (!(options && options.model)) {
+    if (!(options && options.model))
       throw new Error('model is not specified.');
-    }
+
+    this.model.on('change', this.render, this);
+  },
+
+  events: {
+    'click #toggle': 'onClickToggle'
+  },
+
+  onClickToggle: function() {
+    this.model.toggle();
+    console.log(this.model.toJSON());
   },
 
   render: function() {
-    this.$el.html(this.model.escape('description')); // prevent scripting attacks..
+    this.$el.toggleClass('completed', this.model.get('isCompleted'));
+    
+    var checked = this.model.get("isCompleted") ? 'checked' : '';
+    this.$el.html("<input id='toggle' type='checkbox'" + checked + "></input>" + 
+        this.model.escape('description'));
 
     return this;
   }
